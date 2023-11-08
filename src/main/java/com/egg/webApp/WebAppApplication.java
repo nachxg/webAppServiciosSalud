217f85dab153d1b518892d540b3c3d2990267be2
Nacho Gimenez <129122200+nachxg@users.noreply.github.com>
2023-11-08 20:03:36 -0300

    Nacho: Primer commit, creacion del proyecto

---
+++ src/main/java/com/egg/webApp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.egg.webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
