b213e54bb7a95db165157e0754104db2c28a8f16
Luciano Veras <luchoraulveras@gmail.com>
2023-11-10 20:07:26 -0300

    Lombok completo

--- src/main/java/com/egg/webApp/entidades/HistoriaClinica.java
+++ src/main/java/com/egg/webApp/entidades/HistoriaClinica.java
@@ -5,10 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class HistoriaClinica {
