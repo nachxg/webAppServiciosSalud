5832be16e06ea9eb9944d2c36456c28f2a75d285
Diego Veras <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>
2023-11-10 21:15:28 -0300

    Actulizando cllase Administrador

--- src/main/java/com/egg/webApp/entidades/Administrador.java
+++ src/main/java/com/egg/webApp/entidades/Administrador.java
@@ -1,9 +1,17 @@
-//
-//package com.egg.webApp.entidades;
-//
-//import javax.persistence.Entity;
-//
-//@Entity
-//public class Administrador {
-//    
-//}
+
+package com.egg.webApp.entidades;
+
+import com.egg.webApp.enumeraciones.Rol;
+public class Administrador {
+    public static void darBajaUsuario(Usuario usuario) {
+        usuario.setActivo(false);
+    }
+
+    public static void darAltaUsuario(Usuario usuario) {
+        usuario.setActivo(true);
+    }
+
+    public static void establecerROlUsuario(Usuario usuario, String rol) {
+        usuario.setRol(Rol.valueOf(rol));
+    }
+}
