428d0a0a92f9713637dccbeb9e1373ff8058f277
Octavio Palacios <127260323+OctavioPala@users.noreply.github.com>
2023-11-15 21:54:10 -0300

    VALIDACION EN HISTORIAL

--- src/main/java/com/egg/webApp/servicios/HistorialClinicoServicio.java
+++ src/main/java/com/egg/webApp/servicios/HistorialClinicoServicio.java
@@ -3,6 +3,7 @@ package com.egg.webApp.servicios;
 import com.egg.webApp.entidades.HistoriaClinica;
 import com.egg.webApp.entidades.Paciente;
 import com.egg.webApp.entidades.Profesional;
+import com.egg.webApp.excepciones.MiExcepcion;
 import com.egg.webApp.repositorios.HistoiralClinicoRepositorio;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -14,13 +15,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HistorialClinicoServicio {
-    
+
     @Autowired
     private HistoiralClinicoRepositorio histoiralClinicoRepositorio;
-    
+
     @Transactional
-    public void crearHistorial(String diagnostico, String tratamiento, String medicacion, String indicaciones, String estudios, String observaciones, LocalDateTime fecha, Paciente paciente, Profesional profesional) throws Exception {
+    public void crearHistorial(String diagnostico, String tratamiento, String medicacion, String indicaciones, String estudios, String observaciones, LocalDateTime fecha, Paciente paciente, Profesional profesional) throws MiExcepcion {
+
         HistoriaClinica historialClinico = new HistoriaClinica();
+        validar(paciente, profesional);
         historialClinico.setDiagnostico(diagnostico);
         historialClinico.setTratamiento(tratamiento);
         historialClinico.setMedicacion(medicacion);
@@ -32,11 +35,12 @@ public class HistorialClinicoServicio {
         historialClinico.setProfesional(profesional);
         histoiralClinicoRepositorio.save(historialClinico);
     }
-    
+
     @Transactional
-    public void modificarHistorial(Long id, String diagnostico, String tratamiento, String medicacion, String indicaciones, String estudios, String observaciones, LocalDateTime fecha, Paciente paciente, Profesional profesional) throws Exception {
-        
+    public void modificarHistorial(Long id, String diagnostico, String tratamiento, String medicacion, String indicaciones, String estudios, String observaciones, LocalDateTime fecha, Paciente paciente, Profesional profesional) throws MiExcepcion {
+
         Optional<HistoriaClinica> respuesta = histoiralClinicoRepositorio.findById(id);
+        validar(paciente, profesional);
         if (respuesta.isPresent()) {
             HistoriaClinica historialClinico = respuesta.get();
             historialClinico.setDiagnostico(diagnostico);
@@ -51,14 +55,23 @@ public class HistorialClinicoServicio {
             histoiralClinicoRepositorio.save(historialClinico);
         }
     }
-    
+
     public List<HistoriaClinica> listaDeHistorialesClinicosPorPaciente(Paciente paciente) {
         List<HistoriaClinica> historiales = new ArrayList();
         historiales = histoiralClinicoRepositorio.buscarHistorialesPorId(paciente.getId());
         return historiales;
     }
-    
+
     public HistoriaClinica getOne(Long id) {
         return histoiralClinicoRepositorio.getOne(id);
+    }
+
+    private void validar(Paciente paciente, Profesional profesional) throws MiExcepcion {
+        if (paciente == null) {
+            throw new MiExcepcion("el paciente no puede ser nulo");
+        }
+        if (profesional == null) {
+            throw new MiExcepcion("el profecional no puede ser nulo");
+        }
     }
 }
