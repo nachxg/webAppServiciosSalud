64eaaa9e5242e34cd6179fdb483545816ab79066
Diego Veras <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>
2023-11-16 21:40:24 -0300

    Actualizar

--- src/main/java/com/egg/webApp/controladores/PortalControlador.java
+++ src/main/java/com/egg/webApp/controladores/PortalControlador.java
@@ -1,30 +1,32 @@
 package com.egg.webApp.controladores;
 
-<<<<<<< HEAD
-=======
+
 import com.egg.webApp.entidades.Paciente;
 import com.egg.webApp.entidades.Profesional;
 import com.egg.webApp.entidades.Usuario;
->>>>>>> 8547b25e8057f5757241fd457b275703cae50651
+
 import com.egg.webApp.enumeraciones.Sexo;
 import com.egg.webApp.repositorios.PacienteRepositorio;
 import com.egg.webApp.repositorios.UsuarioRepositorio;
 import com.egg.webApp.servicios.EnumServicio;
 import com.egg.webApp.servicios.PacienteServicio;
-<<<<<<< HEAD
-=======
+
+
 import com.egg.webApp.servicios.ProfesionalServicio;
 import com.egg.webApp.servicios.UsuarioServicio;
->>>>>>> 8547b25e8057f5757241fd457b275703cae50651
+
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-=======
+
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
->>>>>>> 7144b931cd489a32de1570259484d5c2faf1d2cf
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -42,11 +44,8 @@ public class PortalControlador {
     @Autowired
     EnumServicio enumServicio;
 
-<<<<<<< HEAD
-    @GetMapping("/")
-=======
+
     @GetMapping("/index")
->>>>>>> 8547b25e8057f5757241fd457b275703cae50651
     public String index(/*ModelMap modelo*/) throws Exception {
 
 
@@ -61,15 +60,8 @@ public class PortalControlador {
         List<Sexo> generos = enumServicio.obtenerGeneros();
         modelo.addAttribute("generos", generos);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         return "registrarPrueba.html";
-=======
-        return ".html"; //TODO: ACTUALIZAR ESTO
->>>>>>> 7144b931cd489a32de1570259484d5c2faf1d2cf
-=======
-        return "registrarPrueba.html";
->>>>>>> 8547b25e8057f5757241fd457b275703cae50651
+
     }
 
     @PostMapping("/") //TODO: ACTUALIZAR ESTO
@@ -77,36 +69,22 @@ public class PortalControlador {
                            @RequestParam String password2, String dni, String sexo, String fechaNacimiento) {
 
         try {
-<<<<<<< HEAD
-            pacienteServicio.registrarPaciente(nombre, apellido, dni, password, password2, sexo);
-            return "redirect:/inicio";
-=======
 
-<<<<<<< HEAD
             pacienteServicio.registrarPaciente(nombre, apellido, dni, password, password2, sexo, fechaNacimiento);
 
             return "redirect:/inicio.html";
->>>>>>> 7144b931cd489a32de1570259484d5c2faf1d2cf
-=======
-            return "redirect:/";
->>>>>>> 8547b25e8057f5757241fd457b275703cae50651
+
 
         } catch (Exception e) {
             System.out.println("ERROR ERROR USUARIO NO CREADO");
             System.out.println(e.getMessage());
-<<<<<<< HEAD
-<<<<<<< HEAD
-            return "inicio.html";
-=======
-            return ".html"; //TODO: ACTUALIZAR ESTO
-=======
-            return "redirect:/registrar";
->>>>>>> 8547b25e8057f5757241fd457b275703cae50651
+            return "inicio.html"; //TODO: ACTUALIZAR ESTO
+
 
         }
     }
 
-<<<<<<< HEAD
+
     @GetMapping("/inicio")
     public String inicio() {
         return "inicio.html";
@@ -125,11 +103,7 @@ public class PortalControlador {
         return ".html"; //TODO: ACTUALIZAR ESTO
     }
 
-    @PostMapping("/perfil/{id}")
-    public String actualizar(MultipartFile archivo, @RequestParam String email, @RequestParam String fechaNacimiento,
-                             @RequestParam String password, @RequestParam String password2,
-                             ModelMap modelo, String telefono, String sexo, @PathVariable Long id) {
-=======
+
     @GetMapping("/perfil/paciente") // TODO: Hay que solucionar, no podemos entrar a perfil
     public String perfilPaciente(ModelMap modelo, HttpSession session) {
         Paciente paciente = (Paciente) session.getAttribute("usuariosession");
@@ -140,7 +114,6 @@ public class PortalControlador {
     @PostMapping("/perfil/paciente/{id}") // TODO: Hay que solucionar, no podemos entrar a perfil
     public String actualizar(MultipartFile archivo, @PathVariable Long id, @RequestParam String email, @RequestParam String fechaNacimiento,
                              @RequestParam String password, @RequestParam String password2, ModelMap modelo, String telefono, String sexo) {
->>>>>>> 8547b25e8057f5757241fd457b275703cae50651
 
         try {
 
@@ -150,17 +123,10 @@ public class PortalControlador {
         } catch (Exception e) {
             System.out.println("ERROR ERROR ");
             System.out.println(e.getMessage());
-
-<<<<<<< HEAD
-            return ".html"; //TODO: ACTUALIZAR ESTO
->>>>>>> 7144b931cd489a32de1570259484d5c2faf1d2cf
-=======
             return "editarPerfilPaciente.html";
->>>>>>> 8547b25e8057f5757241fd457b275703cae50651
         }
     }
-<<<<<<< HEAD
-=======
+
     @GetMapping("/perfil/profesional") // TODO: Hay que solucionar, no podemos entrar a perfil
     public String perfilProfesional(ModelMap modelo, HttpSession session) {
 
@@ -192,18 +158,6 @@ public class PortalControlador {
 
     @GetMapping("/login") // TODO: Hay que solucionar el login, tira eror. No se puedo acceder
     public String login(@RequestParam(required = false) String error, ModelMap modelo) {
-        if (error != null) {
-            modelo.put("error", "ERROR, usuario o contraseña invalidos");
->>>>>>> 8547b25e8057f5757241fd457b275703cae50651
-
-<<<<<<< HEAD
-
-
-    @GetMapping("/login") // TODO: Hay que solucionar el login, tira eror. No se puedo acceder
-=======
-    @GetMapping("/login")
->>>>>>> 7144b931cd489a32de1570259484d5c2faf1d2cf
-    public String login(@RequestParam(required = false) String error, ModelMap modelo) {
 
         if (error != null) {
 
@@ -211,13 +165,7 @@ public class PortalControlador {
         }
         return "login.html";
     }
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 7144b931cd489a32de1570259484d5c2faf1d2cf
 }
-    
     
    
 
