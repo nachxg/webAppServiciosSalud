6db80b7e1b5e9c46108d424c226fe8ac00bc6450
Diego Veras <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>
2023-11-25 23:06:54 -0300

    actulizando

--- src/main/java/com/egg/webApp/controladores/PacienteControlador.java
+++ src/main/java/com/egg/webApp/controladores/PacienteControlador.java
@@ -67,7 +67,7 @@ public class PacienteControlador {
             paciente = (Paciente) session.getAttribute("usuariosession");
         }
        
-        modelo.put("paciente", paciente.getNumeroObraSocial());
+        modelo.put("paciente", paciente);
         return "editarPaciente.html";
     }
     
