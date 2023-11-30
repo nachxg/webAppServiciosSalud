099e34aecc8bf9764e29df45eed4924e38cea56c
Nacho Gimenez <129122200+nachxg@users.noreply.github.com>
2023-11-30 03:56:02 -0300

    Calendario para turnos en inicio profesional

--- src/main/resources/static/js/scriptInicio.js
+++ src/main/resources/static/js/scriptInicio.js
@@ -4,9 +4,9 @@ const desplegable = document.getElementById("desplegable");
 
 let estadoFlecha = false;
 
-contPerfil.addEventListener("click", function(){
+contPerfil.addEventListener("click", function () {
 
-    if(!estadoFlecha){
+    if (!estadoFlecha) {
         contPerfil.classList.add("link-click");
         icono.classList.add("rotar");
         desplegable.style.maxHeight = "20%";
@@ -21,5 +21,134 @@ contPerfil.addEventListener("click", function(){
         desplegable.style.opacity = "0";
         estadoFlecha = false;
     }
-    
+
 });
+
+const daysTag = document.querySelector(".days"),
+    currentDate = document.querySelector(".current-date"),
+    prevNextIcon = document.querySelectorAll(".icons span");
+// getting new date, current year and month
+let date = new Date(),
+    currYear = date.getFullYear(),
+    currMonth = date.getMonth();
+// storing full name of all months in array
+const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+    "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+const renderCalendar = () => {
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+
+    let liTag = "";
+
+    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    }
+
+    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+        // adding active class to li if the current day, month, and year matched
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+            && currYear === new Date().getFullYear() ? "actual" : "";
+        liTag += `<li class="${isToday}" onclick="selectDate(this)">${i}</li>`;
+    }
+
+    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+    }
+
+    const prevIcon = document.querySelector('#prev');
+    const fechaAct = new Date();
+    prevIcon.style.display = currMonth === fechaAct.getMonth() ? "none" : "block";
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+    daysTag.innerHTML = liTag;
+
+    const inputHoy = document.querySelector('#hoy');
+    inputHoy.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="#1D7874" class="bi bi-circle-fill mb-1" viewBox="0 0 16 16">
+    <circle cx="8" cy="8" r="8"/>
+  </svg> Fecha de hoy: ${fechaAct.getDate()}/${fechaAct.getMonth()+1}/${fechaAct.getFullYear()}`;
+
+}
+
+prevNextIcon.forEach(icon => { // getting prev and next icons
+    icon.addEventListener("click", () => { // adding click event on both icons
+        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+        if (currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+            // creating a new date of current year & month and pass it as date value
+            date = new Date(currYear, currMonth, new Date().getDate());
+            currYear = date.getFullYear(); // updating current year with new date year
+            currMonth = date.getMonth(); // updating current month with new date month
+        } else {
+            date = new Date(); // pass the current date as date value
+        }
+
+        renderCalendar(); // calling renderCalendar function
+    });
+});
+
+renderCalendar();
+
+function selectDate(diaElegido) {
+
+    const fechaActual = new Date().setHours(0,0,0,0);
+    const fechaElegida = new Date(currYear, currMonth, diaElegido.textContent);
+
+    if (fechaElegida < fechaActual) {
+        alert('No se puede elegir una fecha anterior a la fecha actual.');
+    } else if(fechaElegida >= fechaActual){
+
+        const dias = document.querySelectorAll('.days li');
+        const inputSelec = document.querySelector('#selec');
+        const inputFecha = document.querySelector('#fecha-seleccionada');
+
+        if (diaElegido.classList.contains('active')) {
+            diaElegido.classList.remove('active');
+            inputFecha.value = '';
+            inputSelec.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="#a068b6" class="bi bi-circle-fill mb-1" viewBox="0 0 16 16">
+            <circle cx="8" cy="8" r="8"/>
+            </svg> Fecha seleccionada: `;
+        } else {
+
+            dias.forEach(dia => {
+                dia.classList.remove('active');
+            });
+            diaElegido.classList.add('active');
+            let dia = diaElegido.textContent;
+            let fecha = `${currYear}/${currMonth + 1}/${dia}`;
+
+            inputSelec.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="#a068b6" class="bi bi-circle-fill mb-1" viewBox="0 0 16 16">
+            <circle cx="8" cy="8" r="8"/>
+            </svg> Fecha seleccionada: ${dia}/${currMonth + 1}/${currYear}`;
+
+            inputFecha.value = fecha;
+
+        }
+
+    }
+    
+}
+
+const horarios = document.querySelectorAll('.item');
+
+  horarios.forEach(function (horario) {
+    horario.addEventListener('click', function () {
+
+        let horarioSeleccionado = '';
+        
+        if(horario.classList.contains('seleccionado')){
+            horario.classList.remove('seleccionado');
+            horarioSeleccionado = '';
+            document.querySelector('#horario-seleccionado').value = horarioSeleccionado;
+        } else {
+            horarios.forEach(horario => {horario.classList.remove('seleccionado')});
+            horario.classList.add('seleccionado');      
+            horarioSeleccionado = horario.textContent;
+            document.querySelector('#horario-seleccionado').value = horarioSeleccionado;   
+        }
+        
+        console.log(document.querySelector('#horario-seleccionado').value);
+    });
+  });
+
