dd42228c850ad3bfd986df83152de5536ce1bcc5
Diego Veras <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>
2023-12-04 21:57:29 -0300

    resolver conflitos

--- src/main/java/com/egg/webApp/servicios/FamiliarServicio.java
+++ src/main/java/com/egg/webApp/servicios/FamiliarServicio.java
@@ -37,7 +37,7 @@ public class FamiliarServicio {
                                  String sexo, LocalDate fechaNacimiento) throws Exception {
         try {
 
-            pacienteServicio.validar(nombre, apellido, dni, password, password2, fechaNacimiento);
+            //pacienteServicio.validar(nombre, apellido, dni, password, password2, fechaNacimiento);
 
             Paciente nuevoMiembro = new Paciente();
             nuevoMiembro.setNombre(nombre);
