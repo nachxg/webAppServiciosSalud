b04f60b281261da182424b38051ad6c8a06f666e
Octavio Palacios <127260323+OctavioPala@users.noreply.github.com>
2023-12-04 21:15:40 -0300

    Update HistorialClinicoControlador.java

--- src/main/java/com/egg/webApp/controladores/HistorialClinicoControlador.java
+++ src/main/java/com/egg/webApp/controladores/HistorialClinicoControlador.java
@@ -1,5 +1,28 @@
 package com.egg.webApp.controladores;
 
+import com.egg.webApp.servicios.HistorialClinicoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/historial_clinico")
 public class HistorialClinicoControlador {
+    
+    @Autowired
+    private HistorialClinicoServicio historialClinicoServicio;
+    
+    @PostMapping("/buscar_historial_clinico/{dni}")
+    public String buscarHistoriales(ModelMap modelo, @RequestParam String dni){
+        
+        try {
+            
+        } catch (Exception e) {
+        }
+        return "";
+    }
 
 }
